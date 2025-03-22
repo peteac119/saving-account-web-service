6eca3a64233ed9dcf7bada21e1886224913e97aa
Pichan Vasantakitkumjorn <pichanv@gmail.com>
2025-03-22 11:02:25 +0700

    Add more test for saving account service

--- src/test/java/org/pete/service/SavingAccountServiceTest.java
+++ src/test/java/org/pete/service/SavingAccountServiceTest.java
@@ -13,6 +13,7 @@ import org.pete.repository.SavingAccountRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
@@ -34,8 +35,7 @@ public class SavingAccountServiceTest {
     @Nested
     public class CreateSavingAccountTestSuite {
         @Test
-        public void should_create_account_successfully() {
-            System.out.println("should_create_account_successfully() name  =>  " + Thread.currentThread().getName());
+        public void should_create_account_with_deposit_amount_successfully() {
             ArgumentCaptor<SavingAccount> savingAccountArgumentCaptor = ArgumentCaptor.forClass(SavingAccount.class);
             CreateSavingAccountRequest mockRequest = new CreateSavingAccountRequest(
                     "ชื่อภาษาไทย",
@@ -61,8 +61,33 @@ public class SavingAccountServiceTest {
         }
 
         @Test
+        public void should_create_account_with_initial_amount_successfully() {
+            ArgumentCaptor<SavingAccount> savingAccountArgumentCaptor = ArgumentCaptor.forClass(SavingAccount.class);
+            CreateSavingAccountRequest mockRequest = new CreateSavingAccountRequest(
+                    "ชื่อภาษาไทย",
+                    "testEnglishNameSuccess",
+                    "2235485123123",
+                    null
+            );
+            Customer mockCustomer = new Customer();
+            Long mockAccountNumber = 1000000L;
+            when(mockCustomerRepository.findOneByThaiNameAndEnglishNameAndCitizenId(
+                    mockRequest.getThaiName(),
+                    mockRequest.getEnglishName(),
+                    mockRequest.getCitizenId()
+            )).thenReturn(mockCustomer);
+            when(mockSavingAccountRepository.save(any(SavingAccount.class))).thenAnswer(input -> input.getArguments()[0]);
+            when(mockSavingAccountRepository.nextAccountNumber()).thenReturn(mockAccountNumber);
+
+            CreateSavingAccountResult actualResult = savingAccountService.createSavingAccount(mockRequest);
+
+            assertThat(actualResult, instanceOf(CreateSavingAccountResult.Success.class));
+            verify(mockSavingAccountRepository, times(1)).save(savingAccountArgumentCaptor.capture());
+            assertSavingAccountInfo(mockRequest, savingAccountArgumentCaptor.getValue(), mockAccountNumber, mockCustomer);
+        }
+
+        @Test
         public void should_return_customer_not_found_if_customer_does_not_exist() {
-            System.out.println("should_return_customer_not_found_if_customer_does_not_exist() name  =>  " + Thread.currentThread().getName());
             CreateSavingAccountRequest mockRequest = new CreateSavingAccountRequest(
                     "ชื่อภาษาไทยที่ไม่มีจริง",
                     "testInvalidEnglishName",
@@ -85,7 +110,8 @@ public class SavingAccountServiceTest {
                                              SavingAccount savingAccount,
                                              Long mockAccountNumber,
                                              Customer mockCustomer) {
-            assertEquals(mockRequest.getDepositAmount(), savingAccount.getBalance());
+            BigDecimal expectedBalance = Optional.ofNullable(mockRequest.getDepositAmount()).orElse(BigDecimal.ZERO);
+            assertEquals(expectedBalance, savingAccount.getBalance());
             assertEquals(mockAccountNumber.toString(), savingAccount.getAccountNumber());
             assertEquals(mockCustomer, savingAccount.getCustomer());
         }
